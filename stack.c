bc784fac86f75b0568eabf876b2458197409be7c
aminamohsen <amina.mohsen4004@gmail.com>
2023-10-18 23:38:13 +0300

    monty monty

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_stack - Prints the top
+ * @head: stack head
+ * @counter: Line_number
+ * Return: No return
+*/
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
